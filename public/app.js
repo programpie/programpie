31f213e2e4394656aa9e6326ff565c3d13b45492
Andreja Kecan <kecan.andreja@gmail.com>
2018-05-10 16:56:54 +0200

    api key

--- public/app.js
+++ public/app.js
@@ -1,10 +1,3 @@
-document.addEventListener("DOMContentLoaded", event =>{
-
-    const app = firebase.app();
-    console.log(app)
-
-});
-
 function googleLogin(){
     const provider = new firebase.auth.GoogleAuthProvider();
 
